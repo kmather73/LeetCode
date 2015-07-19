c7a9baafe58e33e337f72198b30da830aa52b5ec
kmather73 <kmather73@users.noreply.github.com>
2015-07-19 14:10:39 -0500

    Update LowestCommonAncestorOf_a_BinaryTree.java
    Used an arrayList instead of a linkedList for a performance boost.

--- Java/LowestCommonAncestorOf_a_BinaryTree.java
+++ Java/LowestCommonAncestorOf_a_BinaryTree.java
@@ -1,12 +1,48 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
+//This updated version uses a arrayList to make use of random access to speed things up by about a factor of 1.7 
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        ArrayList<TreeNode> pathToP = findPathToNode(root, p);
+        ArrayList<TreeNode> pathToQ = findPathToNode(root, q);
+        
+        int size = Math.min(pathToP.size(), pathToQ.size() );
+        for(int i = 0; i < size; ++i){
+            if(pathToP.get(i) != pathToQ.get(i))
+                return pathToP.get(i-1);
+        }
+        return pathToP.get(size - 1);
+    }
+    
+    public ArrayList<TreeNode> findPathToNode(TreeNode root, TreeNode p){
+        ArrayList<TreeNode> path = new ArrayList<TreeNode>();
+        Stack<TreeNode> myStack = new Stack<TreeNode>();
+        myStack.push(root);
+        
+        TreeNode current = null;
+        
+        while(current != p){
+            current = myStack.pop();
+            
+            if(current != null){
+                path.add(current);
+                myStack.push(current.right);
+                myStack.push(current.left);
+            }
+            else{
+                while(myStack.peek() == null)
+                    myStack.pop();
+                
+                current = myStack.peek();
+                while(path.get(path.size()-1).right != current)
+                    path.remove(path.size()-1);
+                current = null;
+            }
+        }
+        return path;
+    }
+}
+
+//This is the original version that ues a linked list; which makes it kinda slow.
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         LinkedList<TreeNode> pathToP = findPathToNode(root, p);
