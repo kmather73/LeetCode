561ebbc943dad3f957a6cdfbac08d698acd06f1b
kmather73 <kmather73@users.noreply.github.com>
2015-09-12 18:35:54 -0500

    Update PerfectSquares.java

--- Java/PerfectSquares.java
+++ Java/PerfectSquares.java
@@ -1,3 +1,27 @@
+//Newer version using only bit tricks for a little bit more preformance.
+public class Solution {
+    public int numSquares(int n) {
+        int m = n;
+        while( (m&3) == 0 ) // m % 4 == 0
+            m = m>>2;       // m = m / 4
+        if( (m&7) == 7)     // m % 8 == 7
+            return 4;
+        
+        int sqrtOfn = (int) Math.sqrt(n);
+        if(sqrtOfn * sqrtOfn == n)//Is it a Perfect square?
+            return 1;
+            
+        for(int i = 1; i <= sqrtOfn; ++i){
+            int remainder = n - i*i;
+            int sqrtOfNum = (int) Math.sqrt(remainder);
+            if(sqrtOfNum * sqrtOfNum == remainder)
+                return 2;
+            }
+         return 3;
+    }
+}
+
+//Older version
 public class Solution {
     public int numSquares(int n) {
         int m = n;
