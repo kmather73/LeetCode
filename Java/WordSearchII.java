7b014f9b11c0bd806968622bfa6eb22990649fe0
kmather73 <kmather73@users.noreply.github.com>
2015-09-12 21:13:13 -0500

    Update WordSearchII.java

--- Java/WordSearchII.java
+++ Java/WordSearchII.java
@@ -13,49 +13,52 @@ public class Solution {
             return validWords;
         }
         
-        HashMap<Character,List<position>> index = buildIndex(board);//build the index of where characters in the board are.
+        HashMap<Character, List<position>> index = buildIndex(board);//build the index of where characters in the board are.
         boolean[][] visited = new boolean[board.length][board[0].length];//Makes a table of place visited.
         Set<String> validWordSet = new HashSet<String>();//Since we don't want duplicated words will store in set first.
         
-        for(String word : words){ //Checks which words are in the board.
-            if(exist(board, word, visited, index)){ validWordSet.add(word);}
-        }
-        return new ArrayList<String>(validWordSet);//conver the set to a list.
+        for(String word : words) //Checks which words are in the board.
+            if( exist(board, word, visited, index) )
+                validWordSet.add(word);
+        
+        return new ArrayList<String>(validWordSet);//convert the set to a list.
     }
     
-    /* This function find if a given word is in the grid. Here we process words from there last to first characters for better 
-    * performace due to how english words.
+    /* This function finds if a given word is in the grid. Here we process words from there last to first characters for better 
+    * performace due to a heuristic of how english words are formed.
     */
     public boolean exist(char[][] board, String word, boolean[][] visited, HashMap<Character, List<position>> index) {
-        if(word == null || word == ""){
+        if(word == null || word == "")
             return true;
-        }
-
-        List<position> startingPoints = index.get(word.charAt(word.length()-1));/*Find all possible starting point in the board for the
-        * given word.*/
-        if(startingPoints == null || startingPoints.size() == 0){return false;} //The last character is not in the board.
         
-        for(position p : startingPoints){
-            visited[p.row][p.col] = true; // mark the given position as seen.
-            if( exists(board, word, visited, p, word.length()-2)){
-                visited[p.row][p.col] = false; // unmark the given position. We may need this for another word.
+        //Find all possible starting point in the board for the given word.
+        List<position> startingPoints = index.get( word.charAt( word.length() - 1 ) );
+        
+        if(startingPoints == null || startingPoints.size() == 0)
+            return false; //The last character is not on the board.
+        
+        for(position pos : startingPoints){
+            visited[pos.row][pos.col] = true; // mark the given position as seen.
+            if( existsUtill(board, word, visited, pos, word.length() - 2) ){
+                visited[pos.row][pos.col] = false; // unmark the given position. We may need this for another word.
                 return true;
             }
-            visited[p.row][p.col] = false; // unmark the given position. We may need this character later.
+            visited[pos.row][pos.col] = false; // unmark the given position. We may need this character later.
         }
         return false;//faild to find the given word.
     }
     
-    // The wrapper function that does all real work to see if a given is in the grid.
-    // This function starts to check words from there last character to there first character.
-    public boolean exists(char[][] board, String word, boolean[][] visited, position pos, int currLength){
-        if(currLength == - 1){return true;}//base case there are no more characters let in the given word to check.
+    /* The wrapper function that does all real work to see if a given letter is adjacent to a given stating position.
+       This function starts to check words from there last character to there first character. */
+    public boolean existsUtill(char[][] board, String word, boolean[][] visited, position pos, int currLength){
+        if(currLength == - 1) //base case there are no more characters let in the given word to check.
+            return true; 
         
         List<position> nextMoves = validMoves(board, visited, pos, word.charAt(currLength)); 
         for(position move : nextMoves){
             if(!visited[move.row][move.col]){
                visited[move.row][move.col] = true; // mark the given position as seen.
-               if ( exists(board, word, visited, move, currLength - 1) ){
+               if ( existsUtill(board, word, visited, move, currLength - 1) ){
                    visited[move.row][move.col] = false; //Need to unmark this character, maybe needed for another word.
                    return true;
                }
@@ -68,14 +71,15 @@ public class Solution {
     //This function generates a maping from characters to their respective position in the board.
     public HashMap<Character, List<position>> buildIndex(char[][] board){
         HashMap<Character,List<position>> index = new HashMap<Character, List<position>>();
+        
         for(int i = 0; i != board.length; ++i){
-            for(int j = 0; j!= board[0].length; ++j){
-                if(index.containsKey(board[i][j])){// Have we encountered this character before so add the position to the list.
+            for(int j = 0; j != board[0].length; ++j){
+                if( index.containsKey( board[i][j] ) ) // Have encountered this character before so add the position to the list.
                     index.get(board[i][j]).add(new position(i,j));
-                }
-                else{// Have we not encountered this character before so make a new list.
+                    
+                else{// Have not encountered this character before.
                     List<position> list = new ArrayList<position>();
-                    list.add(new position(i,j));
+                    list.add(new position(i, j) );
                     index.put(board[i][j], list );
                 }
             }
@@ -88,21 +92,21 @@ public class Solution {
         List<position> moves = new ArrayList<position>();
         
         //Is the characeter below it admissible?
-        if(currPos.row -1 >= 0 && nextChar == board[currPos.row - 1][currPos.col]){
+        if(currPos.row -1 >= 0 && nextChar == board[currPos.row - 1][currPos.col])
             moves.add(new position(currPos.row - 1, currPos.col ));
-        }
+        
         //Is the characeter below it admissible?
-        if(currPos.row + 1 < board.length  &&  nextChar == board[currPos.row + 1][currPos.col]){
+        if(currPos.row + 1 < board.length  &&  nextChar == board[currPos.row + 1][currPos.col])
             moves.add(new position(currPos.row + 1, currPos.col));
-        }
+        
         //Is the character to the left admissible?
-        if(currPos.col - 1 >= 0 && nextChar == board[currPos.row][currPos.col - 1]){
+        if(currPos.col - 1 >= 0 && nextChar == board[currPos.row][currPos.col - 1])
             moves.add(new position(currPos.row, currPos.col - 1));
-        }
+            
         //Is the character to the right admissible?
-        if(currPos.col + 1< board[0].length && nextChar == board[currPos.row][currPos.col + 1]){
+        if(currPos.col + 1 < board[0].length && nextChar == board[currPos.row][currPos.col + 1])
             moves.add(new position(currPos.row, currPos.col + 1));
-        }
+        
         return moves;
     }
 }
