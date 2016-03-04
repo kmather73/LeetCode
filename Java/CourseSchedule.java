c67e01c24e3a590f1c6cbe32b0437565f9c7c1c9
Kevin <kmather73@users.noreply.github.com>
2016-03-04 17:03:19 -0600

    Update CourseSchedule.java

--- Java/CourseSchedule.java
+++ Java/CourseSchedule.java
@@ -1,5 +1,7 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites.length == 0)
+            return true;
         int n = numCourses;
         int[][] graph = new int[n][n];
         
@@ -14,7 +16,7 @@ public class Solution {
     }
 
     public boolean detectCycle(int[][] graph){
-        int n graph.length;
+        int n = graph.length;
         
         int[] topOrder = new int[n];
         int[] dfNum = new int[n];
@@ -23,23 +25,28 @@ public class Solution {
             dfNum[i] = -1;
         } 
         
-        for(int i = 0; i < n && !cycle[0]; )
-    
+        boolean[] cycle = {false};
+        int[] currTopOrder = {n};
+        int[] currDFNum = {0};
+        
+        for(int i = 0; i < n && !cycle[0]; ++i){
+            DFS(graph, i, topOrder, dfNum, currTopOrder, currDFNum, cycle);
+        }
+        return cycle[0];
     }
     
-    public void DFS(int[][] graph, int node, int[] topOrder, int[] currTopOrder, int[] currDFNum, boolean[] cycle){
+    public void DFS(int[][] graph, int node, int[] topOrder, int[] dfNum, int[] currTopOrder, int[] currDFNum, boolean[] cycle){
         if(dfNum[node] != -1 || cycle[0]) {
-            cycle[0] = true;
             return;
         }
         
-        ++currDfNum[0];
+        ++currDFNum[0];
         dfNum[node] = currDFNum[0];
         
         for(int i = 0; i < graph.length && !cycle[0]; ++i) {
-            if(graph[node][i] != 0 && topOrder[i] == -1) {
-                DFS(graph, i, topOrder);
-            } else if (topOrder[i] != -1){
+            if(graph[node][i] != 0 && dfNum[i] == -1) {
+                DFS(graph, i, topOrder, dfNum, currTopOrder, currDFNum, cycle);
+            } else if(graph[node][i] != 0 && dfNum[i] != -1 && topOrder[i] == -1){
                 cycle[0] = true;
             }
         }
