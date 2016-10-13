2f2d4468784555a8e6a3dfa2d27320f25f6e0960
Kevin <kmather73@users.noreply.github.com>
2016-10-12 20:07:40 -0500

    Update ValidParentheses.java

--- Java/ValidParentheses.java
+++ Java/ValidParentheses.java
@@ -1,22 +1,21 @@
 public class Solution {
     public boolean isValid(String s) {
-        if(s.length() % 2 != 0){return false;}
+        if (s.length() % 2 != 0) return false;
     
-        HashMap<Character,Character> matchingParensMap = new HashMap<Character, Character>();//Build thecorrespondence between the parentheses
- 
+        HashMap<Character,Character> matchingParensMap = new HashMap<Character, Character>();
         matchingParensMap.put(')' , '(');
         matchingParensMap.put('}' , '{');
         matchingParensMap.put(']' , '[');  
     
-        Stack<Character> stackOfParen = new Stack<Character>();//Stack to hold the running set of left parentheses.
+        Stack<Character> stackOfParen = new Stack<Character>();
         
-        for(int i = 0; i < s.length(); ++i){
-            if(matchingParensMap.containsValue( s.charAt(i) ) ){//If we see a left paren keep track of it.
-                stackOfParen.push(s.charAt(i));
-            }
-            else if( stackOfParen.empty() || matchingParensMap.get(s.charAt(i)) != stackOfParen.pop() )// Does the right paren mack with the last seen left paren?
+        for (char ch : s) {
+            if(matchingParensMap.containsValue( ch ) {
+                stackOfParen.push( ch );
+            } else if( stackOfParen.empty() || matchingParensMap.get( ch ) != stackOfParen.pop() ) {
                 return false;
+            }
         }
-        return stackOfParen.empty();//Do we have any left parentheses left?
+        return stackOfParen.empty();
     }
 }
