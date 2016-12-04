1001b6f81469b5182871057e8fcccfb43e7dcedb
Kevin <kmather73@users.noreply.github.com>
2016-12-03 22:34:45 -0600

    Create Number of Segments in a String.cpp

---
+++ C++/Number of Segments in a String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        s = s+" ";
+        int count = 0;
+        for(int i=1;i<s.size(); ++i){
+            if(s[i-1] != ' ' && s[i] == ' ') ++count;
+        }
+        
+        return count;
+    }
+};
