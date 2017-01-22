aa06f6642bcc67ef49cb28506547c11bd06e8ff0
Kevin <kmather73@users.noreply.github.com>
2017-01-21 22:40:43 -0600

    Update Predict the Winner.cpp

--- C++/Predict the Winner.cpp
+++ C++/Predict the Winner.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
         if(nums.size()% 2 == 0) return true;
+        
         int n = nums.size();
-        vector<vector<long>> dp(n, vector<long>(n, -1));
-        int f= 0;
-        long best = utill(nums, dp, 0, n-1, f);
+        vector<vector<int>> dp(n, vector<int>(n, -1));
         
-        long a = 0;
-        if(f) a = utill(nums, dp, 1,n-1, f); 
-        else a = utill(nums, dp, 0, n-2, f);
-        
-        return best >= a;
+        int myBest = utill(nums, dp, 0, n-1);
+        return 2*myBest >= accumulate(nums.begin(), nums.end(), 0);
     }
     
-    long utill(vector<int>& v, vector<vector<long>> &dp, int i, int j, int& f){
-        if(i > v.size() || i > j) return 0;
-        if(i == j) return v[i];
+    int utill(vector<int>& v, vector<vector<int>> &dp, int i, int j){
+        if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         
-        int a = v[i] + min(utill(v,dp, i+1, j-1, f), utill(v, dp, i+2, j, f));
-        int b = v[j] + min(utill(v,dp,i, j-2, f), utill(v,dp, i+1, j-1, f));
+        int a = v[i] + min(utill(v,dp, i+1, j-1), utill(v, dp, i+2, j));
+        int b = v[j] + min(utill(v,dp,i, j-2), utill(v,dp, i+1, j-1));
         dp[i][j] = max(a, b);
                         
-        
-        f = a > b;
         return dp[i][j];
     }
 };
