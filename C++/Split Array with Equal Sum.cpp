32d4db83e5e64146b01bff5e5ea37297006223b5
Kevin <kmather73@users.noreply.github.com>
2017-04-02 18:46:49 -0500

    Update Split Array with Equal Sum.cpp

--- C++/Split Array with Equal Sum.cpp
+++ C++/Split Array with Equal Sum.cpp
@@ -1,26 +1,23 @@
-
 class Solution {
 public:
     bool splitArray(vector<int>& nums) {
-        vector<long> sums{nums[0]};
         unordered_map<long, vector<int>> jump;
         jump[nums[0]].push_back(0);
         
         for (int i=1; i < nums.size(); ++i) {
-            sums.push_back( nums[i] + sums.back());
-            jump[sums.back()].push_back(i);
+            nums[i] += nums[i-1]; 
+            jump[nums[i]].push_back(i);
         }
         
         for (int endIdx = nums.size() - 2; endIdx >= 4; --endIdx) {
-            long endSum = sums.back() - sums[endIdx];
-            
-            for (int startIdx : jump[endSum]) {
-                if (startIdx < endIdx) {
-                    for (int j = startIdx + 1; j + 1 < endIdx; ++j) {
-                        long secondSum = sums[j] - sums[startIdx+1];
-                        long thridSum = sums[endIdx-1] - sums[j+1];
-                        if (secondSum == thridSum && secondSum == endSum) return true;
-                    }
+            long endSum = nums.back() - nums[endIdx];
+    
+            for (int startIdx: jump[endSum]) {
+                for (int j = startIdx + 1; j+1 < endIdx; ++j) {
+                    long secondSum = nums[j] - nums[startIdx+1];
+                    long thridSum = nums[endIdx-1] - nums[j+1];
+                        
+                    if (secondSum == thridSum && secondSum == endSum) return true;
                 }
             }
         }
