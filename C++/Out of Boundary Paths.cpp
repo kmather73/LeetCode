b5288e9e1a710a0907b4616da662d4732cf9d694
Kevin <kmather73@users.noreply.github.com>
2017-05-06 21:44:37 -0500

    Create Out of Boundary Paths.cpp

---
+++ C++/Out of Boundary Paths.cpp
@@ -0,0 +1,41 @@
+class Solution {
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0 ,0};
+
+public:
+    long findPaths(int m, int n, int N, int i, int j) {
+        int mod = (10E8) + 7;
+        vector<vector<vector<long>>> dp(m, vector<vector<long>>(n, vector<long>(N+1)));
+        
+        for(int i=0; i<m; ++i){
+            ++dp[i][0][1];
+            ++dp[i][n-1][1];
+        }
+        
+        for(int j=0; j<n; ++j){
+            ++dp[0][j][1];
+            ++dp[m-1][j][1];
+        }
+        
+        
+        for(int k=2; k<=N; ++k){
+            for(int i=0; i<m; ++i){
+                for(int j=0; j<n; ++j){
+                    for(int t=0; t<4; ++t){
+                        if(outOfBounds(m,n, i+dx[t], j+dy[t])){
+                            dp[i][j][k] = (dp[i][j][k]+1L) % mod;
+                        } else {
+                            dp[i][j][k] = (dp[i][j][k] + dp[i+dx[t]][j+dy[t]][k-1]) % mod;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return dp[i][j][N] % mod;
+    }
+    
+    bool outOfBounds(int m, int n, int i, int j){
+        return i < 0 || j < 0 || i >= m || j >= n;
+    }
+};
