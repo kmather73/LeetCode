4cd7b78b65de16db6f19c8daab204d3a5dcc2694
Kevin <kmather73@users.noreply.github.com>
2017-06-19 11:16:20 -0500

    Update Exchange Seats.sql

--- SQL/Exchange Seats.sql
+++ SQL/Exchange Seats.sql
@@ -4,13 +4,9 @@ UNION
     SELECT id + 1 as id, student FROM seat
         WHERE id % 2 = 1 AND
          id NOT IN
-            (SELECT S1.id as id
-                FROM seat as S1 join (select max(id) as id FROM seat) as S2
-                WHERE S1.id % 2 = 1 and S1.id = S2.id ) 
-        
+            (SELECT max(id) as id FROM seat) 
 UNION 
     SELECT S1.id as id, S1.student 
-    FROM seat as S1 join (select max(id) as id, student FROM seat) as S2
+    FROM seat as S1 join (SELECT max(id) as id FROM seat) as S2
         WHERE S1.id % 2 = 1 and S1.id = S2.id
-        
 ORDER BY id
