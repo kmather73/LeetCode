24b9f173b9f10e786b7e819db380afca145537f9
Kevin <kmather73@users.noreply.github.com>
2018-09-23 21:59:32 -0700

    Create Search in a Binary Search Tree.cpp

---
+++ C++/Search in a Binary Search Tree.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root && root->val != val) {
+            if(root->val < val) root = root->right;
+            else root = root->left;
+        }
+        return root;
+    }
+};
