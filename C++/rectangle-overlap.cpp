525d4b68fe4a7d6ead80181937cf00ebd38cdf92
Kevin <kmather73@users.noreply.github.com>
2019-04-17 21:58:03 -0700

    Create rectangle-overlap.cpp

---
+++ C++/rectangle-overlap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        if(rec1[0] > rec2[0]) return isRectangleOverlap(rec2, rec1);
+        
+        if(rec2[0] >= rec1[2]) return false;
+        
+        if(rec2[1] >= rec1[3]) return false;
+        
+        if(rec2[3] <= rec1[1]) return false;
+        
+        if(rec1[2] <= rec2[0]) return false;
+        
+        return true;
+        
+    }
+};
