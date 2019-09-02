6a0070c6f4f1fa57224b38ec78e0a20ee672f6b7
Kevin <kmather73@users.noreply.github.com>
2019-09-01 18:07:21 -0700

    Update prime-arrangement.cpp

--- C++/prime-arrangement.cpp
+++ C++/prime-arrangement.cpp
@@ -2,9 +2,9 @@ class Solution {
 vector<int> Primes{0,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
     
     public:
-    int numPrimeArrangements(int n) 
-    {
-        int i = lower_bound(Primes.begin(), primes.end(), n); while(Primes[i+1] <= n) ++i;
+    int numPrimeArrangements(int n) {
+        int i = 1;
+        while(Primes[i+1] <= n) ++i;
         
         int j = n - i;
         long int total = 1;
